eeee9cc0c702cd002d60f80a01ad09e0a2bb39bc
loco-co <loco2925@gmail.com>
2021-10-16 23:55:27 +0900

    chapter2 end 21.10.16 night

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'), # url하드코딩을 피하기 위한 네이밍
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Answer
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     """
@@ -23,6 +24,31 @@ def answer_create(request, question_id):
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save() # answer 모델의 content를 get해온다
-    return redirect('pybo:detail', question_id=question_id) # redirect는 페이지를 다시 로드하는 함수
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()  # answer 모델의 content를 get해온다
+            return redirect('pybo:detail', question_id=question_id)  # redirect는 페이지를 다시 로드하는 함수
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
