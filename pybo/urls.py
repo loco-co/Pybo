b176b6d2cd27f7a186bbd1ca0d378debf19f7c64
loco-co <loco2925@gmail.com>
2021-11-09 23:56:44 +0900

    Comment Part.1

--- pybo/urls.py
+++ pybo/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
          name='question_delete'),
     path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
+    path('comment/create/question/<int:question_id>/', views.comment_create_question,
+         name='comment_create_question'),
+    path('comment/modify/question/<int:comment_id>/', views.commet_modify_question,
+         name='comment_modify_question'),
+    path('comment/delete/question/<int:comment_id>/', views.commet_delete_question,
+         name='comment_delete_question'),
 ]
